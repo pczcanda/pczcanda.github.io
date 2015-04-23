0f452682ae7ae7466c6525e24b2a1c3cb3e527ad
Paciencia Zinga Canda <p.canda.pc@googlemail.com>
2015-04-23 22:49:02 +0100

    page for video component

---
+++ javascripts/jquery.pczcanda.videoframe.js
@@ -0,0 +1,119 @@
+﻿window.VIDEO = (function (module, $) {
+  "use strict";
+
+  if (!window.console) {
+    window.console = {
+      log: function () {
+      },
+      info: function () {
+      },
+      debug: function () {
+      }
+    }
+  }
+
+  if (typeof module.config === 'undefined') {
+    module.config = {};
+  }
+
+  module.video = (function (module, $) {
+    var submodule = {};
+
+    submodule.loadVideo = function (videoContainer) {
+      if (videoContainer.length == 0) { return false; }
+
+      var video = videoContainer.find('video');
+      var videoEl = video[0];
+      var showingVideo = false;
+
+      var adjustVideo = function (callback) {
+        //check which one is bigger to make it the 100% element
+        video.removeClass("heightRelated").removeClass("widthRelated");
+
+        var vWidth = videoContainer.innerWidth();
+        var vHeight = videoContainer.innerHeight();
+        var ratio = vHeight / vWidth;
+        if (vHeight <= vWidth && ratio < 0.5625) {
+          video.removeClass("heightRelated").addClass('widthRelated');
+          var newHeight = (vWidth * 9) / 16;
+          video.css({
+            'width': "100%",
+            'height': newHeight,
+            'margin-left': 0
+          });
+        }
+        else {
+          video.removeClass("widthRelated").addClass("heightRelated");
+          var newWidth = (vHeight * 16) / 9;
+          video.css({
+            'width': newWidth,
+            'height': "100%",
+            'margin-left': -(Math.abs(newWidth - vWidth) / 2)
+          });
+        }
+
+        if (callback) {
+          callback();
+        }
+      };
+      var showVideo = function () {
+        $(videoEl).css({
+          'visibility': 'visible',
+          'opacity': 1
+        });
+        videoContainer.css({
+          'background-image': 'none'
+        });
+
+        showingVideo = true;
+      };
+      var hideVideo = function () {
+        $(videoEl).css({
+          'visibility': 'hidden',
+          'opacity': 0
+        });
+        videoContainer.css({
+          'background-image': videoContainer.data('img')
+        });
+      };
+      var setupVideo = function () {
+        if (!ZINGA.helpers.getScreenView().match(/mobile|480/g) && !ZINGA.helpers.isTouchDevice()) {
+          adjustVideo(showVideo);
+        }
+        else {
+          hideVideo();
+        }
+      };
+
+      videoEl.onloadeddata = function () {
+        setupVideo();
+        video.addClass('video-loaded');
+      };
+
+      if (!video.hasClass('video-loaded')) {
+        videoEl.src = videoContainer.data("video");
+      }
+      else {
+        setupVideo();
+      }
+
+      $(window).smartresize(function (evt) {
+        setupVideo();
+      });
+    };
+
+
+    submodule.init = function () {
+        submodule.loadVideo($('.video-container'));
+    };
+
+    $(document).ready(function () {
+      submodule.init();
+    });
+
+    return submodule;
+  }(module, $));
+
+  return module;
+
+}(window.VIDEO || {}, window.jQuery));
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,74 @@
+﻿window.ZINGA = (function (module, $) {
+  "use strict";
 
+    if (typeof module.config === 'undefined') {
+      module.config = {};
+    }
+
+    module.helpers = (function (module, $) {
+      var submodule = {};
+    submodule.getScreenView = function() {
+
+      var indicator;
+      if (document.querySelectorAll('.size-indicator').length == 0) {
+        var indicator = document.createElement('div');
+        indicator.className = indicator.className + " size-indicator";
+        document.body.appendChild(indicator);
+      }
+      else {
+        indicator = document.querySelectorAll('.size-indicator')[0];
+      }
+      var screen,
+          indicatorWidth = indicator.offsetWidth;
+
+    switch (indicatorWidth) {
+        case 3:
+        screen = '480-up';
+        break;
+        case 4:
+          screen = '690-up';
+          break;
+        case 5:
+          screen = '768-up';
+          break;
+        case 6:
+          screen = '1000-up';
+          break;
+        case 7:
+          screen = '1224-up';
+          break;
+        case 8:
+          screen = '1366-up';
+          break;
+        case 9:
+          screen = '1440-up';
+          break;
+        case 10:
+          screen = '1600-up';
+          break;
+        default:
+          screen = 'mobile';
+          break;
+      };
+
+      return screen;
+
+    };
+
+    submodule.isTouchDevice = function() {
+      return !!('ontouchstart' in window) || !!window.navigator.msMaxTouchPoints;
+    };
+
+    submodule.init = function () {
+    };
+
+    $(document).ready(function () {
+      submodule.init();
+    });
+
+    return submodule;
+  }(module, $));
+
+  return module;
+
+}(window.ZINGA || {}, window.jQuery));
